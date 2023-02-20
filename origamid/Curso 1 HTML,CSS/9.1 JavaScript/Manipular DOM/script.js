51d3217b13c7df9991e8ba6db3bea8cc85c7b670
Ecrb3 <73610715+Ecrb3@users.noreply.github.com>
2023-02-19 23:30:55 -0300

    Finalizando projeto html css

---
+++ origamid/Curso 1 HTML,CSS/9.1 JavaScript/Manipular DOM/script.js
@@ -0,0 +1,27 @@
+// const nav = document.querySelector("nav");
+// const produtos = document.querySelector(".produtos a");
+
+// console.log(nav);
+// console.log(produtos);
+
+// console.log(produtos.href);
+
+// nav.style.backgroundColor = "black";
+
+const nome = "O senhro dos anéis";
+const ano = 1954;
+const autor = "J. R. R. Tolkein";
+
+const livro = {
+  nome: "O senhro dos anéis",
+  ano: 1954,
+  autor: "J. R. R. Tolkein",
+};
+
+const filme = {
+  nome: "O senhro dos anéis",
+  ano: 2001,
+  autor: "Peter Jackson",
+};
+
+console.log(livro.ano);
